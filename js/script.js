29229b037e6ea968aa60b6f3212210febd1baacc
jucetina <juliethcetina@gmail.com>
2025-09-11 00:00:14 -0500

    se crea programacion menu hamburguesa

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const menuButton = document.querySelector(".menu_mov-button");
+const menuContainer = document.querySelector(".menu_mov-container");
+
+const mobileResolution = window.matchMedia("screen and (max-width: 480px)");
+
+function showHide(){
+    if(menuContainer.classList.contains("is-active")){
+        menuContainer.classList.remove("is-active");
+    }
+    else{
+        menuContainer.classList.add("is-active");
+    }
+}
+
+function resolutionValidation(resolution){
+    if(resolution.matches){
+        menuButton.addEventListener("click", showHide);
+    }
+    else{
+        menuButton.removeEventListener("click", showHide);
+    }
+}
+
+
+resolutionValidation(mobileResolution);
+
